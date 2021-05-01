8b19b713e53e2f964a92bafaca18fe31f070cbf8
cipepser <respepic@gmail.com>
2021-05-01 13:53:42 +0900

    A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+const CYAN: u8 = 0xb;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = CYAN;
+        }
+    }
+
     loop {}
 }
