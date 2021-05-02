55949d8caa07a706dfc097994a28cef7c1628bfb
cipepser <respepic@gmail.com>
2021-05-02 16:09:26 +0900

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("some numbers: {} {}", 42, 1.337);
+    panic!("some panics message");
     loop {}
 }
