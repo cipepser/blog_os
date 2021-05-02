79e8a6ba2c683d955ee66598d4fead4cd5f1eac8
cipepser <respepic@gmail.com>
2021-05-02 15:03:02 +0900

    first half of VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,15 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-const CYAN: u8 = 0xb;
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = CYAN;
-        }
-    }
-
+    vga_buffer::print_something();
     loop {}
 }
